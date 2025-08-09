874c84df05d9f7b924f12f9725adf86c223bbd05
rrvermaa <rrverma316@gmail.com>
2025-08-09 19:41:42 +0530

    Add 404 fallback for GitHub Pages

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { useEffect, Suspense, lazy } from "react";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import {
-  BrowserRouter as Router,
+  HashRouter as Router,
   Routes,
   Route,
   Outlet,
