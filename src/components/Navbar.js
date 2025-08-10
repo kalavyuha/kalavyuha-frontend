a6d8b5574abcd2c56e837394d756ed3dfee845a9
rrvermaa <rrverma316@gmail.com>
2025-08-10 12:43:49 +0530

    Add 404 fallback for GitHub Pages

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -458,7 +458,7 @@ export default function Navbar() {
           />
         </ListItem>
       </List>
-      <a href="/kalavyuha-frontend/business-page" target="_blank" rel="noopener noreferrer" style={{ textDecoration: "none" }}>
+      <a href="/kalavyuha-frontend/#/business-page" target="_blank" rel="noopener noreferrer" style={{ textDecoration: "none" }}>
         <Box sx={{ p: 2 }}>
           <DownloadButton
             fullWidth
